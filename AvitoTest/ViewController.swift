4e2f053c4a269e6769c716dfb80b3e8c1288b6c1
Olya Gavrilko <olyaganeva@gmail.com>
2021-08-30 13:43:03 +0300

    first commit

---
+++ AvitoTest/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AvitoTest
+//
+//  Created by Olya Ganeva on 30.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
